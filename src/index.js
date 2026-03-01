7f98a89309e606ef1185c3510805f1df28346329
nvkhai238 <23127060@student.hcmus.edu.vn>
2026-03-01 10:23:20 +0700

    fix(Khai): Sua loi hardcode session secret

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(methodOverride("_method"));
 app.use(
   session({
-    secret: "x8w3v9p2q1r7s6t5u4z0a8b7c6d5e4f3g2h1j9k8l7m6n5o4p3q2r1s0t9u8v7w6x5y4z3",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }, // false chạy localhost
